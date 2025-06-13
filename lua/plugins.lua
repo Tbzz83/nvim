6c2ee1a26988bef881f18346f63dc6114e54162d
Tobi Azeez <azeezoe@appstate.edu>
2025-06-13 15:32:33 -0400

    Better autoclose

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -66,11 +66,12 @@ return {
       require'window-picker'.setup()
     end,
   },
-  { 
-    "m4xshen/autoclose.nvim",
-    config = function()
-      require("autoclose").setup()
-    end
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
   }
 }
 
