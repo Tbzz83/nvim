63a1a7f047dad98507a3a177bc46b46d29ae13bc
tbzz83 <tbzz83@gmail.com>
2026-05-22 09:03:46 -0400

    updating kitty and colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,16 +3,16 @@ function ColorMyPencils(color)
     --color = color or "gruvbox"
     --color = color or "monokai-nightasty"
     --color = color or "everforest"
-    color = color or "catppuccin"
-    --color = color or "ayu"
+    --color = color or "catppuccin"
+    color = color or "ayu"
     vim.cmd.colorscheme(color)
 
       
---    vim.api.nvim_set_hl(0, "Normal", { guibg=NONE, ctermbg=NONE, bg = "none" })
---    vim.api.nvim_set_hl(0, "NormalFloat", { guibg=NONE, ctermbg=NONE, bg = "none" })
---    vim.api.nvim_set_hl(0, "NormalNC", { guibg=NONE, ctermbg=NONE, bg = "none" })
---    vim.api.nvim_set_hl(0, "NeoTreeNormal", { guibg=NONE, ctermbg=NONE, bg = "none" })
---    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { guibg=NONE, ctermbg=NONE, bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { guibg=NONE, ctermbg=NONE, bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { guibg=NONE, ctermbg=NONE, bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { guibg=NONE, ctermbg=NONE, bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormal", { guibg=NONE, ctermbg=NONE, bg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { guibg=NONE, ctermbg=NONE, bg = "none" })
 end
 
 ColorMyPencils()
